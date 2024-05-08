2e57bc3070ccf34d923ce5f1bf6e3039b9e9c172
dot-mario <kbh1933@gmail.com>
2024-05-08 14:17:41 +0900

    Feat: Create a status update function and enums

--- Seat.py
+++ Seat.py
@@ -1,5 +1,8 @@
+from enums import Status, TimeLimit
+
+
 class Seat:
-    def __init__(self, seat_number, coordinates, status=0, user_id=None):
+    def __init__(self, seat_number: int, coordinates: tuple, status=Status.AVAILABLE, user_id=-1):
         """
         seat_number (int): 
         고유한 자리 식별 번호 
@@ -9,20 +12,93 @@ class Seat:
         0과 1 사이의 정규화된 값을 가져야함.
         Top left: (x1,y1) / Top right: (x2,y2) / Bottom right: (x3,y3) / Bottom left: (x4,y4)
 
-        status_id (int): 
+        status_id (Status):
         현재 자리의 상태를 나타냅니다.
-        0: 예약 가능
-        1: 사용 중
-        2: 무단 이용 중
-        3: 예약 됨, 입실 대기
-        4: 퇴실 예정
-        5: 자리 비움
+        AVAILABLE : 예약 가능
+        IN_USE : 사용 중
+        UNAUTHORIZED_USE : 무단 이용 중
+        RESERVED_WAITING_ENTRY : 예약 됨, 입실 대기
+        CHECKING_OUT : 퇴실 예정
+        TEMPORARILY_EMPTY : 자리 비움
                     
         user_id (int): 
         만약 자리를 사용하고 있다면, 사용중인 사람의 user id
-        사용중이지 않다면, None
+        사용중이지 않다면, -1
         """
         self.seat_number = seat_number
         self.coordinates = coordinates
         self.status = status
         self.user_id = user_id
+
+        self.is_person = False
+        self.is_luggage = False
+        self.waited_time = 0
+
+    def status_update(self):
+        """
+        분당 반복되야하는 함수. status를 업데이트 한다.
+        """
+        if self.status == Status.TEMPORARILY_EMPTY:
+            self.waited_time += 1
+            if self.waited_time > TimeLimit.TEMPORARILY_EMPTY:
+                self.waited_time = 0
+                # 관리자 연락
+                # 이용자 경고, 벌점
+                self.status = Status.CHECKING_OUT
+                return
+        elif self.status == Status.CHECKING_OUT:
+            self.waited_time += 1
+            if self.waited_time > TimeLimit.CHECKING_OUT:
+                self.waited_time = 0
+                # 퇴실 처리
+                self.status = Status.AVAILABLE
+                self.user_id = -1
+                return
+
+        if self.is_person:  # 사람이 있는가?
+            if self.status in (Status.RESERVED_WAITING_ENTRY, Status.IN_USE, Status.CHECKING_OUT):
+                self.status = Status.IN_USE
+                return
+            else:
+                # 관리자 연락
+                self.status = Status.UNAUTHORIZED_USE
+                return
+        else:
+            if self.status == Status.RESERVED_WAITING_ENTRY:
+                self.waited_time += 1
+                if self.waited_time > TimeLimit.RESERVED_WAITING_ENTRY:
+                    self.waited_time = 0
+                    # 퇴실 처리
+                    self.status = Status.AVAILABLE
+                    self.user_id = -1
+                    return
+                else:
+                    self.status = Status.RESERVED_WAITING_ENTRY
+                    return
+            elif self.status == Status.IN_USE or self.status == Status.TEMPORARILY_EMPTY:
+                if self.is_luggage:  # 짐이 있는가?
+                    self.status = Status.TEMPORARILY_EMPTY
+                    return
+                else:
+                    # 퇴실 처리
+                    self.status = Status.AVAILABLE
+                    self.user_id = -1
+                    return
+            elif self.status == Status.CHECKING_OUT:
+                if self.is_luggage:  # 짐이 있는가?
+                    self.status = Status.CHECKING_OUT
+                    return
+                else:
+                    # 퇴실 처리
+                    self.status = Status.AVAILABLE
+                    self.user_id = -1
+                    return
+            elif self.is_luggage:  # 짐이 있는가?
+                # 관리자 연락
+                self.status = Status.UNAUTHORIZED_USE
+                return
+            else:
+                # 퇴실 처리
+                self.status = Status.AVAILABLE
+                self.user_id = -1
+                return
---
+++ enums.py
@@ -0,0 +1,16 @@
+from enum import IntEnum
+
+
+class Status(IntEnum):
+    AVAILABLE = 0  # 예약 가능
+    IN_USE = 1  # 사용 중
+    UNAUTHORIZED_USE = 2  # 무단 이용 중
+    RESERVED_WAITING_ENTRY = 3  # 예약 됨, 입실 대기
+    CHECKING_OUT = 4  # 퇴실 예정
+    TEMPORARILY_EMPTY = 5  # 자리 비움
+
+
+class TimeLimit(IntEnum):
+    RESERVED_WAITING_ENTRY = 5  # k분 제한
+    TEMPORARILY_EMPTY = 5  # n분 제한
+    CHECKING_OUT = 5  # m분 제한
