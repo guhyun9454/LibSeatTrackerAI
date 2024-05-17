8dd213a4277d471895e2b78cecd18bf40167e140
guhyun9454 <guhyun5454@gmail.com>
2024-05-17 19:35:37 +0900

    Create IoU.py

---
+++ web_with_ai/utils/IoU.py
@@ -0,0 +1,18 @@
+def calculate_iou(box1, box2):
+    x1, y1, x2, y2 = box1
+    x1_p, y1_p, x2_p, y2_p = box2
+
+    xi1 = max(x1, x1_p)
+    yi1 = max(y1, y1_p)
+    xi2 = min(x2, x2_p)
+    yi2 = min(y2, y2_p)
+
+    inter_area = max(0, xi2 - xi1) * max(0, yi2 - yi1)
+
+    box1_area = (x2 - x1) * (y2 - y1)
+    box2_area = (x2_p - x1_p) * (y2_p - y1_p)
+
+    union_area = box1_area + box2_area - inter_area
+
+    iou = inter_area / union_area
+    return iou
