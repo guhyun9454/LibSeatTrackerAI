e781638944e9dec319b23a27d48f1b0275b11bd8
KHU D.com 인지 23 신지혜 <135792125+Jihye1005@users.noreply.github.com>
2024-05-24 10:34:05 +0900

    feat: Design modification

--- seat_reservation_app/lib/user_id_screen.dart
+++ seat_reservation_app/lib/user_id_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'seat_status_screen.dart';
 import 'package:flutter/services.dart';
 
@@ -9,75 +10,229 @@ class UserIdScreen extends StatefulWidget {
 
 class _UserIdScreenState extends State<UserIdScreen> {
   TextEditingController _userIdController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
   String? errorMessage;
+  bool isSaved = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSavedUserId();
+  }
+
+  Future<void> _loadSavedUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedUserId = prefs.getString('savedUserId');
+    final saved = prefs.getBool('isSaved') ?? false;
+    if (savedUserId != null && saved) {
+      setState(() {
+        _userIdController.text = savedUserId;
+        isSaved = saved;
+      });
+    } else {
+      setState(() {
+        _userIdController.clear();
+        isSaved = false;
+      });
+    }
+  }
+
+  Future<void> _saveUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('savedUserId', _userIdController.text);
+    await prefs.setBool('isSaved', true);
+  }
+
+  Future<void> _clearSavedUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('savedUserId');
+    await prefs.setBool('isSaved', false);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Scaffold 배경색을 흰색으로 설정
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Text(
           '학번 입력',
-          style: TextStyle(color: Colors.black), // 제목 텍스트 색상 변경
+          style: TextStyle(color: Colors.black),
         ),
-        iconTheme: IconThemeData(color: Colors.black), // 아이콘 색상 변경
+        iconTheme: IconThemeData(color: Colors.black),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              _loadSavedUserId();
+            },
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextField(
-              controller: _userIdController,
-              decoration: InputDecoration(
-                labelText: 'USER ID:',
-                labelStyle: TextStyle(color: Colors.black), // 라벨 텍스트 색상 변경
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black), // 입력창 밑줄 색상 변경
+            SizedBox(height: 170), // 전체적으로 아래로 이동
+            Container(
+              width: double.infinity,
+              height: 60,
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                color: Color(0xFFF0F0F0),
+                border: Border.all(
+                    width: 1, color: Color.fromARGB(255, 101, 67, 67)),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: TextField(
+                        controller: _userIdController,
+                        decoration: InputDecoration(
+                          labelText: 'USER ID:',
+                          labelStyle: TextStyle(color: Colors.black),
+                          border: InputBorder.none,
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        onChanged: (value) {
+                          if (int.tryParse(value) == null && value.isNotEmpty) {
+                            setState(() {
+                              errorMessage = '유효한 학번을 입력해주세요.';
+                            });
+                          } else {
+                            setState(() {
+                              errorMessage = null;
+                            });
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      _userIdController.clear();
+                    },
+                  ),
+                ],
+              ),
+            ),
+            if (errorMessage != null)
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Text(
+                  errorMessage!,
+                  style: TextStyle(color: Colors.red),
                 ),
               ),
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              onChanged: (value) {
-                if (int.tryParse(value) == null && value.isNotEmpty) {
-                  setState(() {
-                    errorMessage = '유효한 학번을 입력해주세요.';
-                  });
-                } else {
-                  setState(() {
-                    errorMessage = null;
-                  });
-                }
-              },
+            Container(
+              width: double.infinity,
+              height: 60,
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                color: Color(0xFFF0F0F0),
+                border: Border.all(
+                    width: 1, color: Color.fromARGB(255, 101, 67, 67)),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: TextField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          labelText: '비밀번호를 입력하세요',
+                          labelStyle: TextStyle(color: Colors.black),
+                          border: InputBorder.none,
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      _passwordController.clear();
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Text(
+                  '아이디 저장',
+                  style: TextStyle(
+                    color: Color(0xFF6E6E6E),
+                    fontSize: 15,
+                  ),
+                ),
+                SizedBox(width: 10),
+                IconButton(
+                  icon: Icon(
+                    isSaved ? Icons.check_box : Icons.check_box_outline_blank,
+                    color: Color(0xFFA40F16),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if (isSaved) {
+                        _clearSavedUserId();
+                        isSaved = false;
+                      } else {
+                        _saveUserId();
+                        isSaved = true;
+                      }
+                    });
+                  },
+                ),
+              ],
             ),
             SizedBox(height: 20),
-            if (errorMessage != null)
-              Text(
-                errorMessage!,
-                style: TextStyle(color: Colors.red),
+            Container(
+              width: double.infinity,
+              height: 50,
+              decoration: ShapeDecoration(
+                color: Color(0xFFA40F16),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3)),
               ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                final userId = int.tryParse(_userIdController.text);
-                if (userId != null) {
-                  setState(() {
-                    errorMessage = null; // 버튼 클릭 시에도 오류 메시지 초기화
-                  });
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SeatStatusScreen(userId: userId),
+              child: Center(
+                child: TextButton(
+                  onPressed: () {
+                    final userId = int.tryParse(_userIdController.text);
+                    if (userId != null) {
+                      setState(() {
+                        errorMessage = null;
+                      });
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              SeatStatusScreen(userId: userId),
+                        ),
+                      );
+                    } else {
+                      setState(() {
+                        errorMessage = '유효한 학번을 입력해주세요.';
+                      });
+                    }
+                  },
+                  child: Text(
+                    '로그인',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
                     ),
-                  );
-                } else {
-                  setState(() {
-                    errorMessage = '유효한 학번을 입력해주세요.';
-                  });
-                }
-              },
-              child: Text('좌석 상태 확인'),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
