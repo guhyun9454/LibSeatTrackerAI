b87f453f894812598a534bee5a19afb117ad4aed
Kwon GuHyeon <guhyun5454@gmail.com>
2024-05-27 02:29:33 +0900

    refactor: move status update function to SeatsManager

--- src/SeatsManager.py
+++ src/SeatsManager.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from .Seat import Seat
+from .SeatStatus import SeatStatus
 from .Colors import get_color, state_colors
 
 class SeatsManager:
@@ -9,7 +10,7 @@ class SeatsManager:
     도식화한 diagram도 가짐.
     """
 
-    def __init__(self, diagram_sz, background_color="white"):
+    def __init__(self, diagram_sz, background_color= "white"):
         self.seats = []
         self.width ,self.height = diagram_sz
         self.background_color = get_color(background_color)
@@ -27,7 +28,7 @@ class SeatsManager:
         """
         self.image = np.full((self.height, self.width, 3), self.background_color, dtype=np.uint8)
         for seat in self.seats:
-            color_name = self.get_color_by_state(seat.status)
+            color_name = state_colors.get(seat.status)
             color = get_color(color_name)
             cv2.fillPoly(self.image, [seat.DetectArea.polygon], color)
             
@@ -49,6 +50,80 @@ class SeatsManager:
             cv2.putText(self.image, status_text, (cx - 100, cy - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.8, get_color("black"), 2)
             cv2.putText(self.image, person_text, (cx - 100, cy + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.8, get_color("black"), 2)
             cv2.putText(self.image, luggage_text, (cx - 100, cy + 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, get_color("black"), 2)
-    
-    def get_color_by_state(self, state):
-        return state_colors.get(state, "black")
+
+        def status_update(Seat: Seat, MAX_WAITING4ENTRY = 5 ,MAX_TEMPORARILY_EMPTY = 5, MAX_CHECKING_OUT = 5, MAX_WITHOUT_LUGGAGE = 5):
+            """
+            분당 반복되야하는 함수. status를 업데이트 한다.
+            """
+            if Seat.status == SeatStatus.TEMPORARILY_EMPTY:
+                Seat.waited_time += 1
+                if Seat.waited_time > MAX_TEMPORARILY_EMPTY:
+                    Seat.waited_time = 0
+                    Seat.report_to_admin() # 관리자 연락
+                    Seat.penalize_user() # 이용자 경고, 벌점
+                    Seat.status = SeatStatus.CHECKING_OUT
+                    return
+            elif Seat.status == SeatStatus.CHECKING_OUT:
+                Seat.waited_time += 1
+                if Seat.waited_time > MAX_CHECKING_OUT:
+                    Seat.waited_time = 0
+                    Seat.check_out() # 퇴실 처리
+                    Seat.status = SeatStatus.AVAILABLE
+                    return
+
+            if Seat.is_person:  # 사람이 있는가?
+                if Seat.status in (SeatStatus.RESERVED_WAITING_ENTRY, SeatStatus.IN_USE, SeatStatus.CHECKING_OUT, SeatStatus.TEMPORARILY_EMPTY):
+                    Seat.status = SeatStatus.IN_USE
+                    Seat.waited_time = 0
+                    return
+                else:
+                    Seat.report_to_admin() # 관리자 연락
+                    Seat.status = SeatStatus.UNAUTHORIZED_USE
+                    return
+            else:
+                if Seat.status == SeatStatus.RESERVED_WAITING_ENTRY:
+                    Seat.waited_time += 1
+                    if Seat.waited_time > MAX_WAITING4ENTRY:
+                        Seat.waited_time = 0
+                        Seat.check_out() # 퇴실 처리
+                        Seat.status = SeatStatus.AVAILABLE
+                        return
+                    else:
+                        Seat.status = SeatStatus.RESERVED_WAITING_ENTRY
+                        return
+                elif Seat.status in (SeatStatus.IN_USE, SeatStatus.TEMPORARILY_EMPTY):
+                    if Seat.is_luggage: # 짐이 있는가?
+                        Seat.luggage_waited_time = 0 # 검토 안됨
+                        Seat.status = SeatStatus.TEMPORARILY_EMPTY
+                        return
+                    else:
+                        Seat.luggage_waited_time += 1
+                        if Seat.luggage_waited_time > MAX_WITHOUT_LUGGAGE: # MAX_WITHOUT_LUGGAGE번의 update 동안 짐이 없으면 자리비움 진행
+                            Seat.luggage_waited_time = 0
+                            Seat.check_out() # 퇴실 처리
+                            Seat.status = SeatStatus.AVAILABLE
+                            return
+                        else:
+                            return
+                elif Seat.status == SeatStatus.CHECKING_OUT:
+                    if Seat.is_luggage: # 짐이 있는가?
+                        Seat.luggage_waited_time = 0 # 검토 안됨
+                        Seat.status = SeatStatus.CHECKING_OUT
+                        return
+                    else:
+                        Seat.luggage_waited_time += 1
+                        if Seat.luggage_waited_time > MAX_WITHOUT_LUGGAGE: # 3번의 update 동안 짐이 없으면 자리비움 진행
+                            Seat.luggage_waited_time = 0
+                            Seat.check_out() # 퇴실 처리
+                            Seat.status = SeatStatus.AVAILABLE
+                            return
+                        else:
+                            return
+                elif Seat.is_luggage: # 짐이 있는가?
+                    Seat.report_to_admin() # 관리자 연락
+                    Seat.status = SeatStatus.UNAUTHORIZED_USE
+                    return
+                else:
+                    Seat.check_out() # 퇴실 처리
+                    Seat.status = SeatStatus.AVAILABLE
+                    return
