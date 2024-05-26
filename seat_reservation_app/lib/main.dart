d5dbf9766820bbe77e092c1a06f8a8618a5d4ecd
KHU D.com 인지 23 신지혜 <135792125+Jihye1005@users.noreply.github.com>
2024-05-26 22:23:02 +0900

    feat: main_page, user_page addition

--- seat_reservation_app/lib/main.dart
+++ seat_reservation_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'user_id_screen.dart';
+import 'main_page.dart';
+import 'user_page.dart';
+import 'reading_room_status_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +12,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: UserIdScreen(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => UserIdScreen(),
+        '/main': (context) => MainPage(),
+        '/user': (context) => UserPage(
+              userId: 0,
+              department: '',
+              name: '',
+              warningCount: 0,
+            ),
+        '/readingroom': (context) => ReadingRoomStatusScreen(userId: 0),
+      },
     );
   }
 }
