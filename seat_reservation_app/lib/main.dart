f19ea6396716a9ed96576e1ed746abd8e6ebb3a2
KHU D.com 인지 23 신지혜 <135792125+Jihye1005@users.noreply.github.com>
2024-05-27 11:14:32 +0900

    feat: add fonts

--- seat_reservation_app/lib/main.dart
+++ seat_reservation_app/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'user_id_screen.dart';
-import 'main_page.dart';
-import 'user_page.dart';
-import 'reading_room_status_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,22 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Seat Reservation App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'NanumGothic', // NanumGothic 폰트 설정
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => UserIdScreen(),
-        '/main': (context) => MainPage(),
-        '/user': (context) => UserPage(
-              userId: 0,
-              department: '',
-              name: '',
-              warningCount: 0,
-            ),
-        '/readingroom': (context) => ReadingRoomStatusScreen(userId: 0),
-      },
+      locale: Locale('ko', 'KR'), // 한국어 로케일 설정
+      home: UserIdScreen(), // 초기 화면을 UserIdScreen으로 설정합니다.
     );
   }
 }
